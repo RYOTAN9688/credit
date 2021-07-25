67c810ffe5ad7d8e076e07c7bdae28960e932503
RYOTAN9688 <ryota8813@gmail.com>
2021-07-25 17:44:09 +0900

    UI完成

--- client/public/js/payment.js
+++ client/public/js/payment.js
@@ -34,7 +34,7 @@ let style = {
 const card = elements.create("card", { style: style });
 //stripe情報をcard-elementに上げる
 card.mount("#card-element")
-
+//入力されたクレジットカード情報をチェック
 card.on("change", ({ error }) => {
     const displayError = document.getElementById("card=errors");
     //エラーの存在を判定する
@@ -52,12 +52,17 @@ const submitButton = document.getElementById("payment-form-submit");
 //ボタンがクリックされるとアクションを実行
 
 submitButton.addEventListener("click", (event) => {
+    //スピナーを表示する
+    displaySpinner();
+
     stripe
         //収集支払い情報に変換する
         .createPaymentMethod("card", card)
         .then((result) => {
             //エラー時の処理
             if (result.error) {
+                onError();
+
                 //成功したときにサーバーサイドに注文情報を送信
             } else {
                 //支払いメソッドIDをリクエストデータに入れる
@@ -76,6 +81,7 @@ submitButton.addEventListener("click", (event) => {
                         return result.json();
                     })
                     .then((response) => {
+                        onComplete();
                         //正常終了
                     })
             }
@@ -83,8 +89,66 @@ submitButton.addEventListener("click", (event) => {
         })
         .catch(() => {
             //エラー発生時
+            onError();
         });
 });
+
+const returnButtonNormal = document.getElementById("return-button-normal");
+const returnButtonError = document.getElementById("return-button-error");
+const returnButtonNotYet = document.getElementById("return-button-not-yet");
+const returnButtonDefault = document.getElementById("return-button-default");
+
+retutnButtonNormal.addEventListener("click", reset);
+returnButtonError.addEventListener("click", reset);
+returnButtonNotYet.addEventListener("click", reset);
+returnButtonDefault.addEventListener("click", reset);
+
+
+const reset = (event) => {
+    hideError();
+    hideMessage();
+    hideButton();
+    hideNotYetMessage();
+
+    card.mount("#card-element");
+
+}
+
+
+//決済処理が正常に終了したときに実行する
+const onComplete = (response) => {
+    shutdown();
+    //スピナー終了
+    hideSpinner();
+    if (response.error) {
+        onError()
+        //エラーがなければ成功メッセージを表示する
+    } else if (response.paymentIntentStatus === "succeeded") {
+        //確定ボタンを押して完了メッセージを表示する
+        displayMessage();
+    } else {
+        //サーバサイドの決済処理にて成功を受けとることができなかったら
+        //やり直しメッセージを表示する
+        displayNotYetMessage();
+    }
+
+}
+
+const onError = () => {
+    shutdown();
+    if (!document.querySelector(".spinner-border").classList.contains("collapse")) {
+        hideSpinner();
+    }
+    //エラーメッセージを表示する
+    displayError();
+}
+
+const shutdown = () => {
+    //stripeのinputタグへの非表示、
+    card.unmount();
+    //注文確定ボタンの非表示を行う
+    hideButton();
+}
 
 //クラスリストにcollapgeを追加すれば表示、削除すれば非表示にする
 
@@ -108,7 +172,7 @@ const displayError = () => {
 }
 
 //成功メッセージ
-const hidePlayMessage = () => {
+const hideMessage = () => {
     document.querySelector(".contents-payment-result").classList.add("collapse");
 }
 
@@ -116,7 +180,7 @@ const displayMessage = () => {
     document.querySelector(".contents-payment-result").classList.remove("collapse");
 }
 //失敗メッセージ
-const hideDisplayNotYetMessage = () => {
+const hideNotYetMessage = () => {
     document.querySelector(".contents-payment-not-yet").classList.add("collapse");
 }
 
